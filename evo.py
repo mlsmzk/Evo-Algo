cf858f93bd0f2e864bd89ec976cb0cc20eb83b61
jonas <jonas.kazlauskas@me.com>
2021-11-15 21:01:39 -0500

    Add mutation

--- evo.py
+++ evo.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 
 class Population:
     def __init__(self):
         self.pop = np.random.randint(0, 1000, 1000)
 
-def eval_fitness(pop, max_kills = 300):
+
+def eval_fitness(pop, max_kills=300):
     to_delete = []
     for i, val in enumerate(pop):
         if i >= max_kills:
@@ -16,23 +18,32 @@ def eval_fitness(pop, max_kills = 300):
     pop = np.delete(pop, to_delete)
     return pop, len(to_delete)
 
+
 def pair(pop):
     pairs = np.array_split(pop, 2)
     for p1, p2 in zip(pairs[0], pairs[1]):
         yield p1, p2
+
 
 def reproduce(pop, num_to_create):
     pair_iter = pair(pop)
     children = []
     for _ in range(num_to_create):
         children.append(np.mean(next(pair_iter)))
-    pop = np.append(pop, children)
+    return mutate(np.append(pop, children))
+
+
+def mutate(pop, num_to_mutate=40):
+    idx = np.random.randint(0, len(pop), num_to_mutate)
+    for i in idx:
+        pop[i] += random.randint(-100, 100)
     return pop
+
 
 def evolve(pop):
     pop, num_killed = eval_fitness(pop)
-    pop = reproduce(pop, num_killed)
-    return pop
+    return reproduce(pop, num_killed)
+
 
 if __name__ == "__main__":
     num_epochs = 500
